a0f9b09de45ed6802742139ed812b3aea47ac1af
Tadeo Pérez Coria <tadeoperezcoria@MacBook-Pro-de-Tadeo.local>
2025-11-24 17:21:38 -0600

    j

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
-const USE_PRODUCTION = true;
+const USE_PRODUCTION = false;
 
 const API_BASE_URL = USE_PRODUCTION
-  ? 'https://apimex.orionadministracion.com/'
+  ? 'https://apisestaciones.orionadministracion.com/'
   : 'http://localhost:3011/';
 
 export default API_BASE_URL;
